302da136ff6688e5e1736abf2cc4ddda22228eb4
Irina Alekseeva <50372200+AlekseevaIrina@users.noreply.github.com>
2019-09-03 21:27:44 +0300

    First

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
 var navMain = document.querySelector('.navigation');
 var navToggle = document.querySelector('.navigation__toggle');
 
+var modalOpen = document.querySelectorAll(".modal-open");
+var modal = document.querySelector(".modal");
+var modalWrapper = document.querySelector(".modal__wrapper");
+
 navMain.classList.remove('navigation--nojs');
 
 navToggle.addEventListener('click', function () {
@@ -11,4 +15,16 @@ navToggle.addEventListener('click', function () {
     navMain.classList.add('navigation--closed');
     navMain.classList.remove('navigation--opened');
   }
+});
+
+modalOpen.forEach(function (node) {
+  node.addEventListener('click', function () {
+    if (modal.classList.contains('modal--closed')) {
+      modal.classList.remove('modal--closed');
+    }
+  });
+});
+
+modalWrapper.addEventListener('click', function () {
+  modal.classList.add('modal--closed');
 });
