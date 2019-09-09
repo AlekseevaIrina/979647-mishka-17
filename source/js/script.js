8f9b8f7199f628de23b269571732ec034f0675b5
Irina Alekseeva <50372200+AlekseevaIrina@users.noreply.github.com>
2019-09-09 23:00:40 +0300

    third try

--- source/js/script.js
+++ source/js/script.js
@@ -1,30 +1,30 @@
-var navMain = document.querySelector('.navigation');
-var navToggle = document.querySelector('.navigation__toggle');
+var navMain = document.querySelector(".navigation");
+var navToggle = document.querySelector(".navigation__toggle");
 
-var modalOpen = document.querySelectorAll('.modal-open');
-var modal = document.querySelector('.modal');
-var modalWrapper = document.querySelector('.modal__wrapper');
+var modalOpen = document.querySelectorAll(".modal-open");
+var modal = document.querySelector(".modal");
+var modalWrapper = document.querySelector(".modal__wrapper");
 
-navMain.classList.remove('navigation--nojs');
+navMain.classList.remove("navigation--nojs");
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('navigation--closed')) {
-    navMain.classList.remove('navigation--closed');
-    navMain.classList.add('navigation--opened');
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("navigation--closed")) {
+    navMain.classList.remove("navigation--closed");
+    navMain.classList.add("navigation--opened");
   } else {
-    navMain.classList.add('navigation--closed');
-    navMain.classList.remove('navigation--opened');
+    navMain.classList.add("navigation--closed");
+    navMain.classList.remove("navigation--opened");
   }
 });
 
 modalOpen.forEach(function (node) {
-  node.addEventListener('click', function () {
-    if (modal.classList.contains('modal--closed')) {
-      modal.classList.remove('modal--closed');
+  node.addEventListener("click", function () {
+    if (modal.classList.contains("modal--closed")) {
+      modal.classList.remove("modal--closed");
     }
   });
 });
 
-modalWrapper.addEventListener('click', function () {
-  modal.classList.add('modal--closed');
+modalWrapper.addEventListener("click", function () {
+  modal.classList.add("modal--closed");
 });
